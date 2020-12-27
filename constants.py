443354634a2c5320c669c750f65c1bb76196bb7b
Aitor Magán <amagan@conwet.com>
2020-12-27 19:26:09 +0100

    Add 25/12 to the list of dates without info

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
 from datetime import date
 
 GRAPH_IMAGE_PATH = "render/d-solo/HukfaHZgk/covid19?orgId=1&panelId=2&width=1000&height=500&tz=Europe%2FMadrid"
-DAYS_WITHOUT_REPORT = [date(2020, 12, 8)]
+DAYS_WITHOUT_REPORT = [date(2020, 12, 8), date(2020, 12, 25)]
